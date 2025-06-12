8f8d28046f85c10354e8f7a528ed4d802a288588
Achref Arabi <achrefarabi414@gmail.com>
2025-06-12 03:44:52 +0200

    Add interactive Tech Stack section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
+import { Projects } from './components/Projects'
+import { TechStack } from './components/TechStack'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
       <Hero />
+      <Projects />
+      <TechStack />
     </div>
   )
 }
-
 export default App
